c053748a4c078bf0ee9d329e94d15d7ed6646427
ugo.vignon <ugo.vignon@gmail.com>
2023-05-23 09:27:34 +0200

    utilisateur, controller, template

---
+++ src/Controller/UtilisateurController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Utilisateur;
+use App\Form\UtilisateurType;
+use App\Repository\UtilisateurRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UtilisateurController extends AbstractController
+{
+    #[Route('/utilisateur', name: 'app_utilisateur')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UtilisateurController.php',
+        ]);
+    }
+
+    #[Route('/utilisateur/connexion', name: 'utilisateur_connexion')]
+    public function getConnexionForm(Request $request, UtilisateurRepository $utilisateurRepository): Response
+    {
+        $utilisateur = new Utilisateur();
+        $form = $this->createForm(UtilisateurType::class, $utilisateur)
+        ->add('submit', SubmitType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $utilisateur = $form->getData();
+            $utilisateurRepository->save($utilisateur, true);
+            return $this->redirectToRoute('app_utilisateur');
+        }
+
+        return $this->render('login.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
